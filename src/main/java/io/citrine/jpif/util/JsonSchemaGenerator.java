db1c0025009d6fb4d8653d761f059630645f7ca6
Kyle <kyle@citrine.io>
2016-02-11 15:51:53 -0800

    add json schema support

---
+++ src/main/java/io/citrine/jpif/util/JsonSchemaGenerator.java
@@ -0,0 +1,28 @@
+package io.citrine.jpif.util;
+
+import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
+import com.fasterxml.jackson.module.jsonSchema.factories.SchemaFactoryWrapper;
+
+import java.io.IOException;
+
+/**
+ * Helper class used to generate JSON schema.
+ *
+ * @author Kyle Michel
+ */
+public class JsonSchemaGenerator {
+
+    /**
+     * Generate the JSON schema for an input object.
+     *
+     * @param clazz Class of the object to get JSON schema for.
+     * @return String with the pretty-printed JSON schema for the input class.
+     */
+    public static String getJsonSchema(Class<?> clazz) throws IOException {
+        SchemaFactoryWrapper visitor = new SchemaFactoryWrapper();
+        PifObjectMapper.getInstance().acceptJsonFormatVisitor(
+                PifObjectMapper.getInstance().constructType(clazz), visitor);
+        JsonSchema jsonSchema = visitor.finalSchema();
+        return PifObjectMapper.getInstance().writerWithDefaultPrettyPrinter().writeValueAsString(jsonSchema);
+    }
+}
