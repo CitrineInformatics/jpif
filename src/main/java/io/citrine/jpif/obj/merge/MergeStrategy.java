28f8cda4aba2b7150f6ac3b583fa0ffda2de6cd5
Sean Paradiso <seanparadiso@gmail.com>
2017-03-07 15:04:57 -0800

    Add support for map merging

--- src/main/java/io/citrine/jpif/obj/merge/MergeStrategy.java
+++ src/main/java/io/citrine/jpif/obj/merge/MergeStrategy.java
@@ -3,7 +3,9 @@ package io.citrine.jpif.obj.merge;
 import io.citrine.jpif.obj.common.Pio;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * An enumeration of supported merge strategies.
@@ -15,14 +17,8 @@ public enum MergeStrategy {
     // Conflicting objects are replaced if the merge source
     // is non-null and all objects in matching lists are appended
     REPLACE_NON_NULL_AND_APPEND {
-        /**
-         * Merge two lists.
-         * @param mergeInto the destination list.
-         * @param mergeFrom the source list.
-         * @return the merge result.
-         */
         @Override
-        public List<Object> mergeLists(List<Object> mergeInto, List<Object> mergeFrom) {
+        public List<Object> merge(List<Object> mergeInto, List<Object> mergeFrom) {
             List<Object> result = new ArrayList<>();
 
             if (mergeInto != null) {
@@ -35,30 +31,24 @@ public enum MergeStrategy {
             return result;
         }
 
-        /**
-         * Merge two Pio objects.
-         *
-         * @param mergeInto the destination object.
-         * @param mergeFrom the source object.
-         * @return the merge result.
-         */
         @Override
-        public Pio mergePio(Pio mergeInto, Pio mergeFrom) {
+        public Pio merge(Pio mergeInto, Pio mergeFrom) {
             if (mergeFrom != null) {
                 return mergeFrom;
             }
             return mergeInto;
         }
 
-        /**
-         * Merge two objects.
-         *
-         * @param mergeInto the destination object.
-         * @param mergeFrom the source object.
-         * @return the merge result.
-         */
         @Override
-        protected Object mergeObject(Object mergeInto, Object mergeFrom) {
+        public Map<String, Object> merge(Map<String, Object> mergeInto, Map<String, Object> mergeFrom) {
+            Map<String, Object> result = new HashMap<>();
+            result.putAll(mergeInto);
+            result.putAll(mergeFrom);
+            return result;
+        }
+
+        @Override
+        public Object merge(Object mergeInto, Object mergeFrom) {
             if (mergeFrom != null) {
                 return mergeFrom;
             }
@@ -73,7 +63,7 @@ public enum MergeStrategy {
      * @param mergeFrom the source list.
      * @return the merge result.
      */
-    public abstract List<Object> mergeLists(List<Object> mergeInto, List<Object> mergeFrom);
+    public abstract List<Object> merge(List<Object> mergeInto, List<Object> mergeFrom);
 
     /**
      * Merge two Pio objects.
@@ -82,7 +72,16 @@ public enum MergeStrategy {
      * @param mergeFrom the source object.
      * @return the merge result.
      */
-    protected abstract Pio mergePio(Pio mergeInto, Pio mergeFrom);
+    public abstract Pio merge(Pio mergeInto, Pio mergeFrom);
+
+    /**
+     * Merge two Maps.
+     *
+     * @param mergeInto the destination object.
+     * @param mergeFrom the source object.
+     * @return the merge result.
+     */
+    public abstract Map<String, Object> merge(Map<String, Object> mergeInto, Map<String, Object> mergeFrom);
 
     /**
      * Merge two objects that are _not_ Pio instances.
@@ -91,20 +90,5 @@ public enum MergeStrategy {
      * @param mergeFrom the source object.
      * @return the merge result.
      */
-    protected abstract Object mergeObject(Object mergeInto, Object mergeFrom);
-
-    /**
-     * Merge two objects as fields from Pio instances.
-     *
-     * @param mergeInto the destination object.
-     * @param mergeFrom the source object.
-     * @return the merge result.
-     */
-    public Object merge(Object mergeInto, Object mergeFrom) {
-        if (mergeInto instanceof Pio) {
-            return mergePio((Pio) mergeInto, (Pio) mergeFrom);
-        } else {
-            return mergeObject(mergeInto, mergeFrom);
-        }
-    }
+    public abstract Object merge(Object mergeInto, Object mergeFrom);
 }
